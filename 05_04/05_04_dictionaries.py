2b5acf32c0068d9534fbf02d2c62de462566da34
ph0123 <cnphuongdavid@gmail.com>
2022-11-06 19:17:07 +0100

    Finish course....

--- 05_04/05_04_dictionaries.py
+++ 05_04/05_04_dictionaries.py
@@ -7,3 +7,6 @@ statesToCapitals["Texas"] = "Austin"
 statesToCapitals["New York"] = "Albany"
 
 print(statesToCapitals["New York"])
+
+for key,value in enumerate(statesToCapitals.items()):
+    print(f'Key: {key} and Value: {value}')
